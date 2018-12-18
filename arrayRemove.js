0cd6fe5fe2bdddbe42f41b02eacaa4c1e4bc0521
Aneudy adames <a4aneudy@gmail.com>
2018-12-18 01:07:13 -0500

    Update arrayRemove.js

--- arrayRemove.js
+++ arrayRemove.js
@@ -5,13 +5,12 @@
 // array_diff([1,2],[1]) == [2]
 // If a value is present in b, all of its occurrences must be removed from the other:
 
-// function array_diff(a, b) {
-//  if(a.length === 0 ){
-//     return a
-//   }
-//   for (var i = 0; i < b.length; i++) {
-//      a = a.filter(val =>  val !== b[i] )
-//   }
-//   return a
-// }
-console.log("Hello World");
+function array_diff(a, b) {
+ if(a.length === 0 ){
+    return a
+  }
+  for (var i = 0; i < b.length; i++) {
+     a = a.filter(val =>  val !== b[i] )
+  }
+  return a
+}
